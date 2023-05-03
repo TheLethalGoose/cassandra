f7d91ee674214f99a45b2026cd68ca9e6e02582f
Fabian Teufel <fabianteufel@me.com>
2023-05-03 03:54:07 +0200

    Updated JsonConverter to support JsonArrays in results

--- src/main/java/de/fh/dortmund/json/JsonConverter.java
+++ src/main/java/de/fh/dortmund/json/JsonConverter.java
@@ -4,24 +4,33 @@ import com.datastax.driver.core.ColumnDefinitions;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.Set;
+
 public class JsonConverter {
 
-    Session session;
-    public JsonConverter(Session session) {
-        this.session = session;
+    public static JsonArray executeQuery(Session session, String query) {
+        ResultSet resultSet = session.execute(query);
+        return resultSetToJsonArray(resultSet);
     }
-
-    public JsonArray executeQuery(String query) {
-        ResultSet rs = session.execute(query);
+    public static JsonArray resultSetToJsonArray(ResultSet resultSet){
         JsonArray result = new JsonArray();
-        for (Row row : rs) {
+        for (Row row : resultSet) {
             JsonObject obj = new JsonObject();
-            for (ColumnDefinitions.Definition def : rs.getColumnDefinitions().asList()) {
+            for (ColumnDefinitions.Definition def : resultSet.getColumnDefinitions().asList()) {
+
+                if(row.getObject(def.getName()) instanceof Set) {
+                    Gson gson = new Gson();
+                    Set<String> set = row.getSet(def.getName(), String.class);
+                    obj.add(def.getName(),  gson.toJsonTree(set));
+                    continue;
+                }
                 obj.add(def.getName(), (JsonElement) row.getObject(def.getName()));
+
             }
             result.add(obj);
         }
