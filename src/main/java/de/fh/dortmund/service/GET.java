4408d5320337326155845500cd495e3cc2a75064
Fabian Teufel <fabianteufel@me.com>
2023-05-03 17:17:49 +0200

    TODO's for PUT/GET

--- src/main/java/de/fh/dortmund/service/GET.java
+++ src/main/java/de/fh/dortmund/service/GET.java
@@ -5,11 +5,11 @@ import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Session;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 import de.fh.dortmund.json.JsonConverter;
 import de.fh.dortmund.model.Question;
-
+import de.fh.dortmund.model.Tag;
+import de.fh.dortmund.model.User;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class GET extends REST {
@@ -30,6 +30,52 @@ public class GET extends REST {
         return JsonConverter.resultSetToJsonArray(resultSet).get(0).getAsJsonObject().get("tags").getAsJsonArray();
     }
 
+    public JsonArray getQuestionsByTag(Tag tag){
+        //TODO
+        return null;
+    }
+    public JsonArray getQuestionsByTag(String tagName){
+        //TODO
+        return null;
+    }
 
+    public JsonArray getQuestionsByUser(String userName){
+        //TODO
+        return null;
+    }
+    public JsonArray getQuestionsByUser(User user){
+        //TODO
+        return null;
+    }
+
+    public JsonArray getAnswersByQuestion(Question question){
+        //TODO
+        return null;
+    }
+    public JsonArray getAnswersByQuestion(UUID idQuestion){
+        //TODO
+        return null;
+    }
+
+    public JsonArray getQuestionByDate(String dateString){
+        //TODO
+        return null;
+    }
+    public JsonArray getQuestionByDate(LocalDateTime date){
+        //TODO
+        return null;
+    }
+    public JsonArray getUserByEmail(String email){
+        //TODO
+        return null;
+    }
+    public JsonArray findUser(User user){
+        //TODO
+        return null;
+    }
+    public JsonArray findUser(UUID uuid){
+        //TODO
+        return null;
+    }
 
 }
--- src/main/java/de/fh/dortmund/service/PUT.java
+++ src/main/java/de/fh/dortmund/service/PUT.java
@@ -1,4 +1,50 @@
 package de.fh.dortmund.service;
 
-public class PUT {
+import com.datastax.driver.core.Session;
+import com.google.gson.JsonArray;
+import de.fh.dortmund.model.Answer;
+import de.fh.dortmund.model.Post;
+import de.fh.dortmund.model.Question;
+
+import java.util.Set;
+import java.util.UUID;
+
+public class PUT extends REST{
+    public PUT(Session session, boolean debug) {
+        super(session, debug);
+    }
+
+    public JsonArray editQuestion(Question question, String alteredContent){
+        //TODO
+        return null;
+    }
+    public JsonArray editQuestion(UUID idQuestion, String alteredContent){
+        //TODO
+        return null;
+    }
+
+    public JsonArray markAnswerAsAccepted(Answer answer){
+        //TODO
+        return null;
+    }
+    public JsonArray markAnswerAsAccepted(UUID idAnswer){
+        //TODO
+        return null;
+    }
+
+    public JsonArray votePost(Post post , boolean upvote){
+        //TODO
+        return null;
+    }
+
+    public JsonArray addRelatedQuestions(Question question , Set<Question> relatedQuestionsSet){
+        //TODO
+        return null;
+    }
+
+    public JsonArray addLinkedQuestions(Question question , Set<Question> linkedQuestionsSet){
+        //TODO
+        return null;
+    }
+
 }
