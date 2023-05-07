ce856bbac49f36f4978302899b761669e89eb0ee
Fabian Teufel <fabianteufel@me.com>
2023-05-07 23:19:07 +0200

    Fix CassandraDataPersistor

--- src/main/java/de/fh/dortmund/cassandra/CassandraDataPersistor.java
+++ src/main/java/de/fh/dortmund/cassandra/CassandraDataPersistor.java
@@ -1,6 +1,7 @@
 package de.fh.dortmund.cassandra;
 
 import com.datastax.driver.core.Session;
+import de.fh.dortmund.helper.Timer;
 import de.fh.dortmund.models.*;
 import de.fh.dortmund.models.cross.TagQuestion;
 import de.fh.dortmund.service.POST;
@@ -13,21 +14,27 @@ import java.util.UUID;
 public class CassandraDataPersistor {
 
     private Session session;
+    private Timer timer = new Timer();
 
     public CassandraDataPersistor(Session session) {
         this.session = session;
     }
 
     public void persistUsers(List<User> users){
+
         POST post = new POST(session, false);
+        System.out.println("Start persisting users");
+        timer.start();
         for (User user : users) {
             post.createUser(user);
         }
+        System.out.println("Persisted " + users.size() + " users in " + timer);
     }
     public void persistQuestions(List<Question> questions, List<Vote> votes, List<Answer> answers, List<TagQuestion> tagQuestions, List<Tag> tags){
 
         POST post = new POST(session, false);
-
+        System.out.println("Start persisting questions");
+        timer.start();
         for (Question question : questions) {
 
             HashSet<Tag> tagsToQuestion = new HashSet<>();
@@ -35,7 +42,7 @@ public class CassandraDataPersistor {
             int nettoVotes = countNettoVotes(votes, question.getId());
 
             for (TagQuestion tagQuestion : tagQuestions) {
-                if(Objects.equals(tagQuestion.getTagId(), question.getId())){
+                if(Objects.equals(tagQuestion.getPostId(), question.getId())){
                     tagsToQuestion.add(tags.stream().filter(tag -> Objects.equals(tag.getId(), tagQuestion.getTagId())).findFirst().get());
                 }
             }
@@ -43,15 +50,17 @@ public class CassandraDataPersistor {
             post.createQuestion(question, tagsToQuestion, nettoVotes, question.getViews(), answerCount);
 
         }
+        System.out.println("Persisted " + questions.size() + " questions in " + timer);
     }
     public void persistAnswers(List<Answer> answers, List<Vote> votes){
 
         POST post = new POST(session, false);
-
+        System.out.println("Start persisting answers");
         for (Answer answer : answers) {
             int nettoVotes = countNettoVotes(votes, answer.getId());
-            post.createAnswer(answer, UUID.fromString(answer.getIdParent()), nettoVotes);
+            post.createAnswer(answer, UUID.fromString(answer.getParentPostId()), nettoVotes);
         }
+        System.out.println("Persisted " + answers.size() + " answers in " + timer);
     }
     private int countNettoVotes(List<Vote> votes, String postId){
         int nettoVotes = 0;
@@ -70,7 +79,7 @@ public class CassandraDataPersistor {
     private int countAnswers(List<Answer> answers, String questionId){
         int answerCount = 0;
         for (Answer answer : answers) {
-            if(Objects.equals(answer.getIdParent(), questionId)){
+            if(Objects.equals(answer.getParentPostId(), questionId)){
                 answerCount++;
             }
         }
